b64281e2994b2b42a0a8dde93885e370fdcb4ae4
Miguel <minayexx@gmail.com>
2025-10-28 09:22:24 -0500

    Modificaciones
    Modificaciones motosRouter y se corrigio el error de vendedores.

--- BACKEND/src/routes/vendedoresRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { sellersEditarController } from '../controllers/sellersEditarController.js'
-
-const router = Router()
-
-router.put('/:idVendedor', sellersEditarController)
-
-export { router as vendedoresRouter }
-
